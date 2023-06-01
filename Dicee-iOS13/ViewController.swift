da1449f0e4e16736aff36b0afb01acebde87d7e3
Mohamed Aboullezz <42489704+Aboullezz@users.noreply.github.com>
2023-06-01 03:33:55 +0300

    Dicee App

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,23 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [UIImage(imageLiteralResourceName: ("DiceOne")), UIImage(imageLiteralResourceName: ("DiceTwo")), UIImage(imageLiteralResourceName: ("DiceThree")), UIImage(imageLiteralResourceName: ("DiceFour")), UIImage(imageLiteralResourceName: ("DiceFive")), UIImage(imageLiteralResourceName: ("DiceSix"))]
+        
+        
+        
+        diceImageView1.image = diceArray.randomElement()
+            
+        diceImageView2.image = diceArray.randomElement()
+              }
+}
+
